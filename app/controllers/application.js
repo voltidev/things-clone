688ed49d6164d6255ac0b757fd6268aa391eb788
Ivan Votti <ivanvotti@gmail.com>
2018-07-30 03:59:32 +0500

    Add shortcut-bar

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,24 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
+import { set } from '@ember/object';
 
 export default Controller.extend({
   taskEditor: service(),
-  isEditing: alias('taskEditor.hasTask')
+  isShortcutBarOpen: false,
+  isEditing: alias('taskEditor.hasTask'),
+
+  actions: {
+    setSearchQuery(query) {
+      set(this, 'query', query);
+    },
+
+    showShortcutBar() {
+      set(this, 'isShortcutBarOpen', true);
+    },
+
+    toggleShortcutBar() {
+      set(this, 'isShortcutBarOpen', !this.isShortcutBarOpen);
+    }
+  }
 });
