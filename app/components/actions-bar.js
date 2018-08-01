c189a3664f9707cddab3b68705ca63b791b71a4d
Ivan Votti <ivanvotti@gmail.com>
2018-08-02 00:49:47 +0500

    Refactor actions-bar

--- app/components/actions-bar.js
+++ app/components/actions-bar.js
@@ -1,61 +1,12 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { on } from '@ember/object/evented';
 import { alias } from '@ember/object/computed';
-import { EKMixin, EKOnInsertMixin, keyUp } from 'ember-keyboard';
-import velocity from 'velocity-animate';
 
-function hideElements(elements) {
-  velocity(elements, { opacity: 0 }, { duration: 100 });
-  return velocity(elements, { height: 0 }, { duration: 200, easing: 'easeOutCubic' });
-}
-
-export default Component.extend(EKMixin, EKOnInsertMixin, {
+export default Component.extend({
   taskEditor: service(),
   taskSelector: service(),
   classNames: ['c-actions-bar', 'js-actions-bar'],
   classNameBindings: ['hasSelected'],
   isEditing: alias('taskEditor.hasTask'),
-  hasSelected: alias('taskSelector.hasTasks'),
-
-  shortcutEditSelected: on(keyUp('Enter'), function() {
-    if (!this.hasSelected) {
-      return;
-    }
-
-    let taskToEdit = this.taskSelector.sortedTasks.firstObject;
-    this.taskEditor.edit(taskToEdit);
-    this.taskSelector.selectOnly(taskToEdit);
-  }),
-
-  shortcutDeleteSelected: on(keyUp('Backspace'), function() {
-    this.deleteSelected();
-  }),
-
-  actions: {
-    editSelected() {
-      if (!this.hasSelected) {
-        return;
-      }
-
-      let taskToEdit = this.taskSelector.sortedTasks.firstObject;
-      this.taskEditor.edit(taskToEdit);
-      this.taskSelector.selectOnly(taskToEdit);
-    },
-
-    deleteSelected() {
-      this.deleteSelected();
-    }
-  },
-
-  deleteSelected() {
-    if (!this.hasSelected) {
-      return;
-    }
-
-    hideElements(document.querySelectorAll('.is-selected .js-task')).then(() => {
-      this.deleteTasks(this.taskSelector.tasks);
-      this.taskSelector.clear();
-    });
-  }
+  hasSelected: alias('taskSelector.hasTasks')
 });
--- app/components/app-container.js
+++ app/components/app-container.js
@@ -3,8 +3,27 @@ import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
 import { set } from '@ember/object';
 import { on } from '@ember/object/evented';
-import { EKMixin, EKOnInsertMixin, keyDown } from 'ember-keyboard';
+import { EKMixin, EKOnInsertMixin, keyDown, keyUp } from 'ember-keyboard';
 import velocity from 'velocity-animate';
+
+function hideElements(elements) {
+  velocity(elements, { opacity: 0 }, { duration: 100 });
+  return velocity(elements, { height: 0 }, { duration: 200, easing: 'easeOutCubic' });
+}
+
+function fadeOutFloatedButton(parentElement) {
+  let floatingBtn = parentElement.querySelector('.js-floating-button');
+  let floatingBtnStyle = floatingBtn.style;
+
+  return velocity(
+    floatingBtn,
+    { translateX: -100, translateY: -100, opacity: 0 },
+    { duration: '200ms', easing: 'easeOutQuart' }
+  ).then(() => {
+    // Recover styles after animation.
+    floatingBtn.style = floatingBtnStyle;
+  });
+}
 
 export default Component.extend(EKMixin, EKOnInsertMixin, {
   taskSelector: service(),
@@ -12,6 +31,7 @@ export default Component.extend(EKMixin, EKOnInsertMixin, {
   classNames: ['l-container'],
   isShortcutsDialogOpen: false,
   isEditing: alias('taskEditor.hasTask'),
+  hasSelected: alias('taskSelector.hasTasks'),
 
   shortcutNewTask: on(keyDown('KeyN'), function() {
     this.createNewTask();
@@ -21,21 +41,42 @@ export default Component.extend(EKMixin, EKOnInsertMixin, {
     this.toggleShortcutsDialog();
   }),
 
-  actions: {
-    createNewTask() {
-      let element = this.element.querySelector('.js-floating-button');
-      velocity(
-        element,
-        { translateX: -100, translateY: -100, opacity: 0 },
-        { duration: '200ms', easing: 'easeOutQuart' }
-      );
-      velocity(element, { translateX: 0, translateY: 0, opacity: 1 });
+  shortcutEditSelected: on(keyUp('Enter'), function() {
+    if (!this.hasSelected) {
+      return;
+    }
 
+    let taskToEdit = this.taskSelector.sortedTasks.firstObject;
+    this.taskEditor.edit(taskToEdit);
+    this.taskSelector.selectOnly(taskToEdit);
+  }),
+
+  shortcutDeleteSelected: on(keyUp('Backspace'), function() {
+    this.deleteSelected();
+  }),
+
+  actions: {
+    toggleShortcutsDialog() {
+      this.toggleShortcutsDialog();
+    },
+
+    createNewTask() {
+      fadeOutFloatedButton(this.element);
       setTimeout(() => this.createNewTask(), 50);
     },
 
-    toggleShortcutsDialog() {
-      this.toggleShortcutsDialog();
+    editSelected() {
+      if (!this.hasSelected) {
+        return;
+      }
+
+      let taskToEdit = this.taskSelector.sortedTasks.firstObject;
+      this.taskEditor.edit(taskToEdit);
+      this.taskSelector.selectOnly(taskToEdit);
+    },
+
+    deleteSelected() {
+      this.deleteSelected();
     }
   },
 
@@ -47,6 +88,17 @@ export default Component.extend(EKMixin, EKOnInsertMixin, {
     this.createTask().then(newTask => {
       this.taskSelector.selectOnly(newTask);
       setTimeout(() => this.taskEditor.edit(newTask));
+    });
+  },
+
+  deleteSelected() {
+    if (!this.hasSelected) {
+      return;
+    }
+
+    hideElements(document.querySelectorAll('.is-selected .js-task')).then(() => {
+      this.deleteTasks(this.taskSelector.tasks);
+      this.taskSelector.clear();
     });
   }
 });
