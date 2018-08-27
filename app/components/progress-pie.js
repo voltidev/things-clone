57d31a5f04459266d8467783a2612dcbf5b399f6
Ivan Votti <ivanvotti@gmail.com>
2018-08-27 17:52:17 +0500

    Fix progress-pie in Safari

--- app/components/progress-pie.js
+++ app/components/progress-pie.js
@@ -1,11 +1,13 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { equal } from '@ember/object/computed';
 
 const CIRCUMFERENCE = 25.1327412287;
 
 export default Component.extend({
   progress: 0,
   CIRCUMFERENCE,
+  isZero: equal('progress', 0),
 
   strokeDasharray: computed('progress', function() {
     return this.progress * CIRCUMFERENCE / 100;
