662de33eaf10e3ae29b0577c15a3e86d994dcb76
Ivan Votti <ivanvotti@gmail.com>
2018-08-28 19:18:25 +0500

    Close move dialog on side click & Esq press

--- app/components/move-dialog.js
+++ app/components/move-dialog.js
@@ -1,4 +1,42 @@
 import Component from '@ember/component';
+import { on } from '@ember/object/evented';
+import { run } from '@ember/runloop';
+import { EKMixin, EKOnInsertMixin, keyDown } from 'ember-keyboard';
 
-export default Component.extend({
+export default Component.extend(EKMixin, EKOnInsertMixin, {
+  classNames: ['c-dialog', 'js-move-dialog'],
+
+  shortcutClose: on(keyDown('Escape'), function() {
+    this.close();
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.closeOnSideClick = this.closeOnSideClick.bind(this);
+  },
+
+  didRender() {
+    this._super(...arguments);
+    this.startHandlingRootClick();
+  },
+
+  willDestroyElement() {
+    this.stopHandlingRootClick();
+  },
+
+  closeOnSideClick({ target }) {
+    run(() => {
+      if (!this.element.querySelector('.js-dialog-body').contains(target)) {
+        this.close();
+      }
+    });
+  },
+
+  startHandlingRootClick() {
+    document.addEventListener('mousedown', this.closeOnSideClick, true);
+  },
+
+  stopHandlingRootClick() {
+    document.removeEventListener('mousedown', this.closeOnSideClick, true);
+  }
 });
