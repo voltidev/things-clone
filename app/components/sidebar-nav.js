102299b8fd8fbbc73eb5b848765b38fc073de199
ivanvotti <ivanvotti@gmail.com>
2018-10-08 23:19:16 +0500

    Fix focusOut

--- app/components/sidebar-nav.js
+++ app/components/sidebar-nav.js
@@ -7,6 +7,17 @@ export default Component.extend({
   data: service(),
   classNames: ['c-sidebar-nav'],
 
+  actions: {
+    fixFocusOut() {
+      // Work-around to trigger focus-out event that is broken by
+      // `ember-sortable` (it calls preventDefault on click).
+      // Without the fix, project form won't work properly when user clicks on any project link-to.
+      if (document.activeElement) {
+        document.activeElement.blur();
+      }
+    }
+  },
+
   * eachTransition({ keptSprites, insertedSprites, removedSprites }) {
     keptSprites.forEach(move);
     insertedSprites.forEach(fadeIn);
