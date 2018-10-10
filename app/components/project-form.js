c34941770d87c8ee8bac435463e7f1ad401a0d4f
ivanvotti <ivanvotti@gmail.com>
2018-10-11 00:53:13 +0500

    Fix project-form autofocus

--- app/components/project-form.js
+++ app/components/project-form.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import fade from 'ember-animated/transitions/fade';
 import { set } from '@ember/object';
+import { scheduleOnce } from '@ember/runloop';
 
 export default Component.extend({
   nameFieldValue: '',
@@ -11,13 +12,9 @@ export default Component.extend({
     this._super(...arguments);
     set(this, 'nameFieldValue', this.project.name);
     set(this, 'notesFieldValue', this.project.notes);
-  },
-
-  didRender() {
-    this._super(...arguments);
 
     if (!this.project.name) {
-      this.focusNameField();
+      scheduleOnce('afterRender', this, this.focusNameField);
     }
   },
 
